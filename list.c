c7ce57138d7dfb3c6757106ebaaf13bcd9481329
Lyudmila Ponomareva <l.ponomareva@g.nsu.ru>
2023-12-16 20:59:48 +0700

    Основной код закончен, но что-то немного не работает:)

--- list.c
+++ list.c
@@ -23,20 +23,58 @@ int thread2 = 0;
 int thread3 = 0;
 int thread_change = 0;
 
+pthread_mutex_t* changer_sync;
+
 
 void* _1_thread(void* arg) {
 	Storage* storage = (Storage*)arg;
 	
 	while(1){
+		printf("0");
 		Node* cur = storage->first;
-		while(cur->next != null){
-			pthread_mutex_lock(cur->sync);
+		pthread_mutex_lock(cur->sync);
+		while(cur->next != NULL){
 			pthread_mutex_lock(cur->next->sync);
-			
+			if(strlen(cur->value) < strlen(cur->next->value)){
+				++thread1;
+			}
+			pthread_mutex_unlock(cur->sync);
+			cur = cur->next;
 		}
-		readwrite блокировку
-		++thread1;
-		сюда rwlock
+	}
+}
+
+void* _2_thread(void* arg) {
+	Storage* storage = (Storage*)arg;
+	
+	while(1){
+		Node* cur = storage->first;
+		pthread_mutex_lock(cur->sync);
+		while(cur->next != NULL){
+			pthread_mutex_lock(cur->next->sync);
+			if(strlen(cur->value) > strlen(cur->next->value)){
+				++thread2;
+			}
+			pthread_mutex_unlock(cur->sync);
+			cur = cur->next;
+		}
+	}
+}
+
+void* _3_thread(void* arg) {
+	Storage* storage = (Storage*)arg;
+	
+	while(1){
+		Node* cur = storage->first;
+		pthread_mutex_lock(cur->sync);
+		while(cur->next != NULL){
+			pthread_mutex_lock(cur->next->sync);
+			if(strlen(cur->value) == strlen(cur->next->value)){
+				++thread3;
+			}
+			pthread_mutex_unlock(cur->sync);
+			cur = cur->next;
+		}
 	}
 }
 
@@ -48,50 +86,70 @@ void* _change_thread(void* arg) {
 		Node* cur = storage->first;
 		pthread_mutex_lock(cur->sync);
 		pthread_mutex_lock(cur->next->sync);
+		pthread_mutex_lock(cur->next->next->sync);
 		if(rand() % 2){
-			Node* tmp = cur;
-			storage->first = cur->next;
-			cur->next = tmp;
+			Node* c = cur;
+			Node* cn = cur->next;
+			Node* cnn = cur->next->next;
 			
-			rwlock
+			storage->first = cn;
+			cn->next = c;
+			c->next = cnn;
+			
+			pthread_mutex_lock(changer_sync);
 			++thread_change;
-			rwlock
+			pthread_mutex_unlock(changer_sync);
 		}
 		pthread_mutex_unlock(cur->sync);
-		pthread_mutex_unlock(cur->next->sync);
+		cur = cur->next;
 		while(1){
-			pthread_mutex_lock(cur->sync);
-			pthread_mutex_lock(cur->next->sync);
 			pthread_mutex_lock(cur->next->next->sync);
 			
 			if(rand() % 2){
-				Node* tmp = cur->next;
-				cur->next = cur->next->next;
-				cur->next->next = tmp;
+				Node* cnnn = cur->next->next->next;
+				Node* cn = cur->next;
+				Node* cnn = cur->next->next;
 				
-				rwlock
+				cur->next = cnn;
+				cur->next->next = cn;
+				cur->next->next->next = cnnn;
+				
+				pthread_mutex_lock(changer_sync);
 				++thread_change;
-				rwlock
+				pthread_mutex_unlock(changer_sync);
 			}
 			
 			pthread_mutex_unlock(cur->sync);
-			pthread_mutex_unlock(cur->next->sync);
-			pthread_mutex_unlock(cur->next->next->sync);
-			if(cur->next->next->next == null){
+			if(cur->next->next->next == NULL){
 				break;
 			}
+			cur = cur->next;
 		}
 	}
 }
 
+void *qmonitor(void *arg) {
+	printf("qmonitor: [%d %d %d]\n", getpid(), getppid(), gettid());
+
+	while (1) {
+		pthread_mutex_lock(changer_sync);
+		printf("thread1: %d   thread2: %d   thread3: %d      thread_change: %d\n", thread1, thread2, thread3, thread_change);
+		pthread_mutex_unlock(changer_sync);
+		sleep(1);
+	}
+
+	return NULL;
+}
+
 int main(){
-	Storage* storage = malloc(Storage);
-	int size = 10;
+	Storage* storage = malloc(sizeof(Storage));
+	int size = 100;
+	int err;
+	pthread_t tid;
 	
 	Node* first = malloc(sizeof(Node));
-	char val[2] = "a";
-	first->value = val;
-	first->next = null;
+	first->value[0] = 'a';
+	first->next = NULL;
 	first->sync = malloc(sizeof(pthread_mutex_t));
 	storage->first = first;
 	
@@ -101,22 +159,56 @@ int main(){
 	for(int i = 0; i < size - 1; ++i){
 		Node* curNew = malloc(sizeof(Node));
 		int len = rand() % 100;
-		char str[len] = "";
-		for(int j = 0; j < len: ++j){
-			str[j] = 'a';
+		for(int j = 0; j < len; ++j){
+			curNew->value[j] = 'a';
 		}
-		curNew->value = str;
 		curNew->sync = malloc(sizeof(pthread_mutex_t));
-		curNew->next = null;
+		curNew->next = NULL;
 		
 		cur->next = curNew;
 		cur = cur->next;
 	}
+	changer_sync = malloc(sizeof(pthread_mutex_t));
 
 	//создание трех потоков
-	
+	err = pthread_create(&tid, NULL, _1_thread, storage);
+	if (err) {
+		printf("main: pthread_create() failed: %s\n", strerror(err));
+		return -1;
+	}
+	err = pthread_create(&tid, NULL, _2_thread, storage);
+	if (err) {
+		printf("main: pthread_create() failed: %s\n", strerror(err));
+		return -1;
+	}
+	err = pthread_create(&tid, NULL, _3_thread, storage);
+	if (err) {
+		printf("main: pthread_create() failed: %s\n", strerror(err));
+		return -1;
+	}
 	
 	//создание трех потоков проверки
+	err = pthread_create(&tid, NULL, _change_thread, storage);
+	if (err) {
+		printf("main: pthread_create() failed: %s\n", strerror(err));
+		return -1;
+	}
+	err = pthread_create(&tid, NULL, _change_thread, storage);
+	if (err) {
+		printf("main: pthread_create() failed: %s\n", strerror(err));
+		return -1;
+	}
+	err = pthread_create(&tid, NULL, _change_thread, storage);
+	if (err) {
+		printf("main: pthread_create() failed: %s\n", strerror(err));
+		return -1;
+	}
+	
+	err = pthread_create(&tid, NULL, qmonitor, NULL);
+	if (err) {
+		printf("main: pthread_create() failed: %s\n", strerror(err));
+		return -1;
+	}
 	
 	return 0;
 }
